60b9895604e6021cb46791404b4ad9f20dfe1a43
L1xian <medazizammar7@gmail.com>
2025-11-15 15:25:16 +0100

    proj

---
+++ PROJ/untitled/src/main/java/org/example/chat/ChatController.java
@@ -0,0 +1,16 @@
+package org.example.chat;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(Message message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + message.getName() + "!");
+    }
+}
